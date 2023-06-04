8390971913a46960326d9e4e8926664c798863fa
irfanfiriezky <irfanfiriezkybahri@gmail.com>
2023-06-04 20:18:27 +0700

    back to start

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#1D1628',
-      'monkey': '#262626',
-      'bermuda-tua': '#130F1B',
-      'ungu-janda': '#392C4F',
-      'kolom' : '#404040',
-      'abu-terang' : '#777777',
-      'login' : '#5D5FEF',
-      'register' : '#E25286',
-      'temu': '#52E2C0',
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.js',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin')
-  ],
-
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+    ]
 }
-
-
